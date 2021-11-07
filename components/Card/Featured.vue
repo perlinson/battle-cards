e87ad1f5aeb2d862eb98fb8dd5c27a2ff1140e6b
perlinson <perlinson_2008@163.com>
2021-11-07 12:42:12 +0800

    增加游戏王卡组编辑器  在此基础上完善自己的编辑器

---
+++ components/Card/Featured.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>{{ card.name }}</h2>
+    <div><small>{{card.type}}</small></div>
+    {{ card.desc }}
+    <br>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      card: Object,
+    }
+  }
+</script>
