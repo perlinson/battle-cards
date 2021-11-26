46f7e37fecfb07d59e48aff6cab6df82d130aea7
perlinson <perlinson_2008@163.com>
2021-11-26 11:35:53 +0800

    feat(plugins/animate.js): 增加animate.css 动画库支持

---
+++ plugins/animate.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+
+
+Vue.prototype.$animate = function(node, animationName, callback) {
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+}
