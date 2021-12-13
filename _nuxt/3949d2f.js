76a72dd301dee5341628bd444d1bbfcb0e1eeae3
perlinson <perlinson_2008@163.com>
2021-12-13 10:46:07 +0000

    [skip ci] Updates

---
+++ _nuxt/3949d2f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{638:function(t,e,n){"use strict";n.r(e);var r={created:function(){this.$router.push("/dashboard/")}},c=n(41),component=Object(c.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
