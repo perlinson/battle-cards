460ac8fb24c5042870166f6edd0eb4a08902b7ce
perlinson <perlinson_2008@163.com>
2021-12-15 12:40:17 +0800

    ci: 更新nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ const routerBase =
     : {}
 
 const { locale, availableLocales, fallbackLocale } = config.locales
-export default {
+module.export = {
   ...routerBase,
 
   target: process.env.NODE_ENV !== 'production' ? 'server' : 'static',
